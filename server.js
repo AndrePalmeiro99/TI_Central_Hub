75c0efcfec393a57931d14e1346fae9a82821d9a
André Palmeiro <andrepalmeiro14@gmail.com>
2026-09-04 09:03:13 -0300

    Feat: Adicionado e-mail proprio de envio para a redefinição de senha

--- server.js
+++ server.js
@@ -25,6 +25,9 @@ const smtpTransporter = nodemailer.createTransport({
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
